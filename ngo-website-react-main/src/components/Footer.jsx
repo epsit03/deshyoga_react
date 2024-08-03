13417ac0cafb77a7172c1f8b440d762429ce6fe5
epsit03 <epsitbhardwaj26@gmail.com>
2024-08-04 01:51:44 +0530

    footer optimised

--- ngo-website-react-main/src/components/Footer.jsx
+++ ngo-website-react-main/src/components/Footer.jsx
@@ -6,167 +6,35 @@ const Footer = () => {
   const [Gallery, setGallery] = useState(false);
   const [Contact, setContact] = useState(false);
   return (
-    <footer>
-  
 
-      <div class="footer">
-        <div class="footer-content">
-          <div class="footer-about">
-            <p> <h1>OUR VISION</h1></p> 
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            <br />
-            </p>
+    <footer className="footer">
+      <div className="footer-content">   
+        <div className="footer-section about">
+          {/* <h1 className="logo-text">Logo</h1> */}
+          <img src="../assets/swami_ji_dp.webp" class="swamijidp"/>
+          
           </div>
 
-          <div class="footer-links">
-            <h2
-              class="links"
-              onClick={() => {
-                setLinks(!Links);
-              }}
-            >
-              QUICK LINKS
-            </h2>
-            <br />
-            <ul class={"footer-links-ul " + (Links && "active-links")}>
-              <li>
-                {/* <i class="fa fa-arrow-circle-right" aria-hidden="true"></i> */}
-                <Link to="/"><div class="footer-l">Home</div></Link>
-              </li>
-              <li>
-                {/* <i class="fa fa-arrow-circle-right" aria-hidden="true"></i> */}
-                <Link to="about"><div class="footer-l">About</div></Link>
-              </li>
-              <li>
-                {/* <i class="fa fa-arrow-circle-right" aria-hidden="true"></i> */}
-                <Link to="causes"><div class="footer-l">Causes</div></Link>
-              </li>
-              <li>
-                {/* <i class="fa fa-arrow-circle-right" aria-hidden="true"></i> */}
-                <Link to="event"><div class="footer-l">Events</div></Link>
-              </li>
-              <li>
-                {/* <i class="fa fa-arrow-circle-right" aria-hidden="true"></i> */}
-                <Link to="news"><div class="footer-l">News</div></Link>
-              </li>
-              <li>
-                {/* <i class="fa fa-arrow-circle-right" aria-hidden="true"></i> */}
-                <Link to="contact"><div class="footer-l">Contact</div></Link>
-              </li>
-            </ul>
-          </div>
-{/* 
-          <div class="gallery">
-            <h2
-              class="gal-btn"
-              onClick={() => {
-                setGallery(!Gallery);
-              }}
-            >
-              GALLERY
-            </h2>
-            <div class={"gal-container " + (Gallery && "gal-container-active")}>
-              <div>
-                <img src="assets/ga1.png" />
-                <img src="assets/ga2.png" />
-              </div>
-              <div>
-                <img src="assets/gap3.png" />
-                <img src="assets/gal4.png" />
-              </div>
-              <div>
-                <img src="assets/gal5.png" />
-                <img src="assets/gal6.png" />
-              </div>
-            </div>
-          </div> */}
-
-              {/* <section class="contact"> 
-        <div class="contact-1">
-          <i class="fa fa-phone" id="call" aria-hidden="true"></i>
-          <p class="question">DO YOU HAVE A QUESTION?</p>
-          <p class="number">+0123 456 789</p>
+        <div className="footer-section">
+        <u><h3>Quick Links</h3></u>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
         </div>
-        <div class="contact-2">
-          <div>
-            <i class="fa fa-envelope-o" id="mail" aria-hidden="true"></i>
-            <input
-              type="search"
-              placeholder="Enter email"
-              className="text-black"
-            />
-          </div>
-          <button 
-            onClick={() => {
-              alert("subscribed!!!");
-            }}
-          >
-            Subscribe
-          </button>
-        </div>
-       </section> */}
-
-          <div class="contact-footer">
-            <h2
-              class="contact-btn"
-              onClick={() => {
-                setContact(!Contact);
-              }}
-            >
-              CONTACT US
-            </h2>
-
-            <div
-              class={
-                "contact-container " + (Contact && "contact-container-active")
-              }
-            >
-              <div class="contact-card">
-                <div class="contact-heading">
-                  <div class="location">
-                    <i class="fa fa-map-marker one" aria-hidden="true"></i>
-                  </div>
-                  <h2>Head Office</h2>
-                </div>
-                <div class="contact-ad">
-                  <p>KH.No 1001, Ground floor, Street No 14/1,
-                  Village Jagatpur Extension</p>
-                  <p>Delhi 110084</p>
-                </div>
-              </div>
-              <div class="contact-card">
-                <div class="contact-heading">
-                  <div class="location">
-                    <i class="fa fa-phone two" aria-hidden="true"></i>
-                  </div>
-                  <h2>Phone Number</h2>
-                </div>
-                <div class="contact-ad">
-                  <p>123, Main Street, Your City</p>
-                  <p>USA 789456</p>
-                </div>
-              </div>
-              <div class="contact-card">
-                <div class="contact-heading">
-                  <div class="location">
-                    <i class="fa fa-envelope three" aria-hidden="true"></i>
-                  </div>
-                  <h2>Email</h2>
-                </div>
-                <div class="contact-ad">
-                  <p>123, Main Street, Your City</p>
-                  <p>USA 789456</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <p class="copyright">
-            Copyright @ 2024. Developed by Epsit Bhardwaj. All Rights Reserved.
-          </p>
+        <div className="footer-section">
+          <u><h3>Contact Us</h3></u>
+          <p>Your address</p>
+          <p>Your email</p>
+          <p>Your phone number</p>
         </div>
       </div>
+      <div className="footer-add">KH.No 1001, Ground floor, Street No 14/1,<br/>
+Village Jagatpur Extension, Delhi 110084<br/>
+deshyoga2017@gmail.com</div>
     </footer>
+    
   );
 };
 
--- ngo-website-react-main/src/components/Membership.jsx
+++ ngo-website-react-main/src/components/Membership.jsx
@@ -8,26 +8,7 @@ import './membership.css';
 const Membership = () => {
   return (
     <div>
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Join Our Membership!</title>
-        <link rel="shortcut icon" href="images/logo.png" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Fredoka+One&family=Kanit:wght@300&display=swap" />
-      </head>
-      <body>
-        <h1>Unlock Exclusive Benefits - Join Now!</h1>
-        <h2>Become a member and gain access to amazing features, discounts, and more!</h2>
-        <div className="membership-options">
-        <Link to="/contributionA" className="btn">Monthly Membership</Link>
-          <Link to="/contributionB" className="btn">Yearly Membership (Save!)</Link>
-          <Link to="/contributionC" className="btn">Lifetime Membership</Link>
-        </div>
-        
-      </body>
-    </div>
+   Hello world</div>
   );
 }
 
